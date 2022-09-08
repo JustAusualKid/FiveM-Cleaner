cb3935326304706f16e0ace3729de2ad33dfb4d3
JustAusualKid <100459240+JustAusualKid@users.noreply.github.com>
2022-09-08 12:07:35 +0100

    Add files via upload

---
+++ xboxCleaner.bat
@@ -0,0 +1,25 @@
+@echo off
+goto :xboxclean
+:xboxclean
+cls
+powershell -Command "& {Get-AppxPackage -AllUsers xbox | Remove-AppxPackage}"
+sc stop XblAuthManager
+sc stop XblGameSave
+sc stop XboxNetApiSvc
+sc stop XboxGipSvc
+sc delete XblAuthManager
+sc delete XblGameSave
+sc delete XboxNetApiSvc
+sc delete XboxGipSvc
+reg delete "HKLM\SYSTEM\CurrentControlSet\Services\xbgm" /f
+schtasks /Change /TN "Microsoft\XblGameSave\XblGameSaveTask" /disable
+schtasks /Change /TN "Microsoft\XblGameSave\XblGameSaveTaskLogon" /disableL
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\GameDVR" /v AllowGameDVR /t REG_DWORD /d 0 /f
+cls
+set hostspath=%windir%\System32\drivers\etc\hosts
+echo 127.0.0.1 xboxlive.com >> %hostspath%
+echo 127.0.0.1 user.auth.xboxlive.com >> %hostspath%
+echo 127.0.0.1 presence-heartbeat.xboxlive.com >> %hostspath%
+rd %userprofile%\AppData\Local\DigitalEntitlements /q /s
+exit
+goto :eof
